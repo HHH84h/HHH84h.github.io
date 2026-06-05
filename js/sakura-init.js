064ce268bcfa3d6b657e55eb91075b560a0f1c28
HHH84h <13791945183@163.com>
2026-06-05 15:53:26 +0800

    Site updated: 2026-06-05 15:53:25

--- js/sakura-init.js
+++ js/sakura-init.js
@@ -1,4 +1,4 @@
-new Sakura('body', {
+new Sakura('html', {
   colors: [
     { gradientColorStart: 'rgba(255, 183, 197, 0.9)', gradientColorEnd: 'rgba(255, 197, 208, 0.9)', gradientColorDegree: 120 },
     { gradientColorStart: 'rgba(255,189,189,0.9)', gradientColorEnd: 'rgba(227,170,181,0.9)', gradientColorDegree: 120 },
