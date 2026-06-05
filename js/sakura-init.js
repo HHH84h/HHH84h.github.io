7d96b75c89cc92e5af36baecca06c2cfe64d89ad
HHH84h <13791945183@163.com>
2026-06-05 16:07:35 +0800

    Site updated: 2026-06-05 16:07:35

--- js/sakura-init.js
+++ js/sakura-init.js
@@ -1,13 +1,18 @@
-new Sakura('html', {
-  colors: [
-    { gradientColorStart: 'rgba(255, 183, 197, 0.9)', gradientColorEnd: 'rgba(255, 197, 208, 0.9)', gradientColorDegree: 120 },
-    { gradientColorStart: 'rgba(255,189,189,0.9)', gradientColorEnd: 'rgba(227,170,181,0.9)', gradientColorDegree: 120 },
-    { gradientColorStart: 'rgba(212,152,163,0.9)', gradientColorEnd: 'rgba(242,185,196,0.9)', gradientColorDegree: 120 }
-  ],
-  blowAnimations: ['blow-soft-left', 'blow-medium-right', 'blow-soft-right', 'blow-medium-left'],
-  className: 'sakura',
-  fallSpeed: 1.5,
-  maxSize: 14,
-  minSize: 8,
-  delay: 300
-});
+(function() {
+  var el = document.querySelector('html');
+  if (typeof Sakura !== 'undefined' && el) {
+    new Sakura('html', {
+      colors: [
+        { gradientColorStart: 'rgba(255, 183, 197, 0.9)', gradientColorEnd: 'rgba(255, 197, 208, 0.9)', gradientColorDegree: 120 },
+        { gradientColorStart: 'rgba(255,189,189,0.9)', gradientColorEnd: 'rgba(227,170,181,0.9)', gradientColorDegree: 120 },
+        { gradientColorStart: 'rgba(212,152,163,0.9)', gradientColorEnd: 'rgba(242,185,196,0.9)', gradientColorDegree: 120 }
+      ],
+      blowAnimations: ['blow-soft-left', 'blow-medium-right', 'blow-soft-right', 'blow-medium-left'],
+      className: 'sakura',
+      fallSpeed: 1.5,
+      maxSize: 14,
+      minSize: 8,
+      delay: 300
+    });
+  }
+})();
